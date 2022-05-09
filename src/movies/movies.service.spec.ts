d4cd4a85115301a80a9000726febfbd6eede33b8
dohyun.lee <lhl890@naver.com>
2022-05-09 22:16:27 +0900

    Add movie CRUD unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -20,6 +22,62 @@ describe('MoviesService', () => {
     it('should be an array', () => {
       const result = service.getAll();
       expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        genres: ['test'],
+        title: 'test',
+        year: 2022,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a movie', () => {
+      service.create({
+        genres: ['test'],
+        title: 'test',
+        year: 2022,
+      });
+
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        genres: ['test'],
+        title: 'test',
+        year: 2022,
+      });
+
+      service.update(1, {
+        title: 'updated title',
+      });
+
+      const afterUpdate = service.getOne(1);
+
+      expect(afterUpdate.title).toEqual('updated title');
     });
   });
 });
